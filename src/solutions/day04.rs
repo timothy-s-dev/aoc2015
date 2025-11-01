ceca58d266e6fd6e12907f7ba20ad3d654534b7a
Timothy Sassone <timothy.s.dev@gmail.com>
2025-11-01 17:35:44 +0000

    Add day04 solutions

--- src/solutions/day04.rs
+++ src/solutions/day04.rs
@@ -4,12 +4,30 @@ use anyhow::Result;
 pub struct Day4;
 
 impl Solution for Day4 {
-    fn part1(&self, _input: &str) -> Result<String> {
-        Err(anyhow::anyhow!("Not implemented"))
+    fn part1(&self, input: &str) -> Result<String> {
+        let mut number = 0;
+        loop {
+            let input = format!("{}{}", input, number);
+            let hash = format!("{:x}", md5::compute(input));
+            if hash.starts_with("00000") {
+                break Ok(number.to_string());
+            }
+            number += 1;
+            if number > 100_000_000 { break Err(anyhow::anyhow!("Not found by 100M"))}
+        }
     }
 
-    fn part2(&self, _input: &str) -> Result<String> {
-        Err(anyhow::anyhow!("Not implemented"))
+    fn part2(&self, input: &str) -> Result<String> {
+        let mut number = 0;
+        loop {
+            let input = format!("{}{}", input, number);
+            let hash = format!("{:x}", md5::compute(input));
+            if hash.starts_with("000000") {
+                break Ok(number.to_string());
+            }
+            number += 1;
+            if number > 100_000_000 { break Err(anyhow::anyhow!("Not found by 100M"))}
+        }
     }
 }
 
@@ -19,7 +37,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn todo() {
-        // Implement me
+    fn test_part1() {
+        assert_eq!(Day4.part1("abcdef").unwrap(), "609043");
+        assert_eq!(Day4.part1("pqrstuv").unwrap(), "1048970");
     }
 }
